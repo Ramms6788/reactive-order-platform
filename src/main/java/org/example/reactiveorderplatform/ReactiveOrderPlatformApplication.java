e2005ccbe5dfbe103ecc275a8d3019a9f37e49b4
oleksiiluchko <oleksii.luchko@loopme.com>
2026-02-25 10:52:50 +0200

    Initialize Spring Boot reactive project with Gradle setup and sample application.

---
+++ src/main/java/org/example/reactiveorderplatform/ReactiveOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.reactiveorderplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveOrderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/reactiveorderplatform/ReactiveOrderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reactiveorderplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveOrderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
